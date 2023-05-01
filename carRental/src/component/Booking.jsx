76200b333dd625d9dac6ae6af464d10df6e85949
basemosman2 <49072081+basemosman2@users.noreply.github.com>
2023-05-02 00:27:49 +0100

    added booking function

--- carRental/src/component/Booking.jsx
+++ carRental/src/component/Booking.jsx
@@ -1,21 +1,57 @@
 import { carData, cities } from "../assets/contents";
 
+import { useNavigate } from "react-router-dom";
+
 import React, { useState } from "react";
 import Datepicker from "react-tailwindcss-datepicker";
 
-import { AiFillCar } from "react-icons/ai";
-import { MdLocationOn } from "react-icons/md";
+import { AiFillCar ,  MdLocationOn} from "react-icons/all";
 import bookBg from "../assets/book-bg.png";
 
 const Booking = () => {
-  const [value, setValue] = useState({
+  const [dateValue, setDateValue] = useState({
     startDate: null,
     endDate: null,
   });
 
-  const handleValueChange = (newValue) => {
-    console.log("newValue:", newValue);
-    setValue(newValue);
+  const [carType, setCarType] = useState(null);
+  const [carPickUp, setCarPickUp] = useState(null);
+  const [carDropDown, setCarDropDown] = useState(null);
+  const [message, setMessage] = useState(null);
+
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, "0");
+  const day = String(today.getDate()).padStart(2, "0");
+  const currentDate = `${year}-${month}-${day}`;
+
+  const navigation = useNavigate();
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+
+    if (
+      carType &&
+      carPickUp &&
+      carDropDown &&
+      dateValue.startDate &&
+      dateValue.endDate
+    ) {
+      const queryParams = new URLSearchParams({
+        carType,
+        carPickUp,
+        carDropDown,
+        ...dateValue,
+      });
+      console.log({ carType, carPickUp, carDropDown, dateValue });
+      setMessage(null);
+      navigation({
+        pathname: "/vehicleModel",
+        search: `?${queryParams.toString()}`,
+      });
+    } else {
+      setMessage("Please Select all the fields");
+    }
   };
 
   return (
@@ -24,22 +60,30 @@ const Booking = () => {
     >
       <img
         src={bookBg}
-        alt="book Background"
+        alt="booking Background"
         className=" absolute inset-0 object-cover w-full h-full -z-0"
       />
-      <div className=" relative z-1 grid md:grid-cols-2 gap-4">
+      {message && (
+        <p className=" mb-4 text-center text-red-600 text-2xl">{message}</p>
+      )}
+      <form
+        onSubmit={onSubmitHandler}
+        className=" relative z-1 grid md:grid-cols-2 gap-4"
+      >
         <div className=" flex items-center border border-gray-300 rounded-sm p-2">
           <AiFillCar className=" text-primary text-xl" />
           <label htmlFor="carSelect"></label>
           <select
             name="carSelect"
             id="carSelect"
+            value={carType}
+            onChange={(e) => setCarType(e.target.value)}
             className=" w-full p-2 bg-transparent text-lg font-semibold text-text outline-none"
           >
-            <option value="SelectYourCarType<">Select Your Car Type</option>
+            <option value="SelectYourCarType">Select Your Car Type</option>
             {carData.map((carSelect) => (
-              <option value={carSelect.name} key={carSelect.id}>
-                {carSelect.name}
+              <option value={carSelect.type} key={carSelect.id}>
+                {carSelect.type}
               </option>
             ))}
           </select>
@@ -50,9 +94,10 @@ const Booking = () => {
           <select
             name="SelectPickUpLocation"
             id="SelectPickUpLocation"
+            onChange={(e) => setCarPickUp(e.target.value)}
             className=" w-full p-2 bg-transparent text-lg font-semibold text-text outline-none"
           >
-            <option value="SelectPickUpLocation<">
+            <option value="SelectPickUpLocation">
               Select Pick Up Location
             </option>
             {cities.map((carSelect, i) => (
@@ -68,6 +113,7 @@ const Booking = () => {
           <select
             name="DropOffLocation"
             id="DropOffLocation"
+            onChange={(e) => setCarDropDown(e.target.value)}
             className=" w-full p-2 bg-transparent text-lg font-semibold text-text outline-none"
           >
             <option value="SelectDropOffLocation">
@@ -84,20 +130,23 @@ const Booking = () => {
           <label htmlFor="datePickUp"></label>
           <Datepicker
             useRange={false}
-            value={value}
-            onChange={handleValueChange}
+            value={dateValue}
             inputClassName=" bg-transparent w-full outline-none text-lg font-semibold text-text"
+            onChange={(newValue) => setDateValue(newValue)}
             placeholder={"Select Date Range ..."}
-            startFrom="2023-01-01"
+            startFrom={currentDate}
             displayFormat={"DD/MM/YYYY"}
             eadOnly={true}
-            minDate={new Date("2023-01-05")}
+            minDate={new Date(currentDate)}
           />
         </div>
-        <button className=" bg-primary py-4 rounded-md text-white text-xl font-semibold tracking-wider">
+        <button
+          to="/vehicleModel"
+          className=" bg-primary py-4 rounded-md text-white text-xl font-semibold tracking-wider"
+        >
           Search
         </button>
-      </div>
+      </form>
     </div>
   );
 };
